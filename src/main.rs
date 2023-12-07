f3e93d17cc4110fb76c7b20e9c6edad474caa76c
Thegaming Life <thegaminglife09@gmail.com>
2023-12-07 11:13:15 +0100

    v2.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-use fs_utils::recursive_copy;
-
-mod fs_utils;
+use std::{
+    fs,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+
+
     let dir = std::fs::read_dir(
-        r#"C:\Users\thega\AppData\Roaming\Ryujinx\bis\user\save\0000000000000001\0"#,
+        r#"C:\Users\thega\AppData\Roaming\yuzu\nand\user\save\0000000000000000\F0748197393EEDCDE71C4EE207801D64\0100F2C0115B6000\"#,
     )
     .unwrap();
 
-    let new_dir = r#"C:\Users\thega\Documents\totk_save_backups\sav_"#.to_owned()
-        + &rand::random::<u128>().to_string();
+    let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
-    std::fs::DirBuilder::new()
-        .recursive(true)
-        .create(&new_dir)
-        .expect("");
+    let new_dir = r#"C:\Users\thega\Documents\totk_save_backups\sav_"#.to_owned()
+        + &rand::random::<u32>().to_string();
+
+    let mut dir_builder = std::fs::DirBuilder::new();
+
+    dir_builder.recursive(true);
+    dir_builder.create(&new_dir).expect("");
+    dir_builder.create(new_dir.clone() + "/picturebook/").expect("");
 
     for file in dir {
         let file = file.unwrap().path();
@@ -22,11 +29,27 @@ fn main() {
         if file.is_dir() {
             match file.file_name().unwrap().to_str().unwrap() {
                 "slot_00" => {
-                    recursive_copy(file, &new_dir);
+                    for item in file.read_dir().unwrap() {
+                        let item = item.unwrap();
+
+                        fs::copy(
+                            item.path(),
+                            new_dir.to_owned() + "/" + item.file_name().to_str().unwrap(),
+                        )
+                        .expect("couldnt copy file");
+                    }
                 }
 
                 "picturebook" => {
-                    recursive_copy(file, &new_dir);
+                    for item in file.read_dir().unwrap() {
+                        let item = item.unwrap();
+
+                        fs::copy(
+                            item.path(),
+                            new_dir.to_owned() + "/picturebook/" + item.file_name().to_str().unwrap(),
+                        )
+                        .expect("couldnt copy file");
+                    }
                 }
 
                 _ => continue,
